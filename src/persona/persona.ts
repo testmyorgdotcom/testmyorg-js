919437cb4cb1fa38c7cbe4137ba80307dc46c08d
Nikita Chursin <chursinn91@gmail.com>
2021-06-13 19:56:30 +0300

    added password prop to persona

--- src/persona/persona.ts
+++ src/persona/persona.ts
@@ -1,9 +1,7 @@
 export class Persona {
-  public readonly personaName: string;
-  public readonly username: string;
-
-  constructor(personaName: string, username?: string) {
-    this.personaName = personaName;
-    this.username = username;
-  }
+  constructor(
+    public readonly personaName: string,
+    public readonly username?: string,
+    public readonly password?: string
+  ) {}
 }
