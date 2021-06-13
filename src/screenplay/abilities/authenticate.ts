ae61e3099df59d934f31ff17629401e5694d89d7
Nikita Chursin <chursinn91@gmail.com>
2021-06-13 16:20:38 +0300

    authenticate - added credentials provider

--- src/screenplay/abilities/authenticate.ts
+++ src/screenplay/abilities/authenticate.ts
@@ -1,25 +1,40 @@
 import { Persona, PersonaManager } from "@/persona";
 import { Ability, Actor, UsesAbilities } from "@serenity-js/core";
 import { context } from "@/context";
-import { Lazy } from "@/utils/lazy";
+import {
+  Credentials,
+  ICredentialsProvider,
+  LightCredentialsProvider,
+} from "@/persona/auth";
 
 export class Authenticate implements Ability {
   private _persona: Persona; // = new Lazy(() => this.personaManager.);
+  private credentialsProvider: ICredentialsProvider;
 
   private constructor(
     private readonly personaName: string,
     private readonly personaManager: PersonaManager
   ) {}
 
+  public credentials(): Credentials {
+    return this.credentialsProvider.getCredentialsFor(this.persona());
+  }
+
   public persona() {
     return this._persona;
   }
 
-  public static asPersonaCalled(
-    personaName: string,
-    personaManager: PersonaManager = context.personaManager
-  ): Ability {
-    return new Authenticate(personaName, personaManager);
+  public withCredentialsProvider(
+    credentialsProvider: ICredentialsProvider
+  ): Authenticate {
+    this.credentialsProvider = credentialsProvider;
+    return this;
+  }
+
+  private defaultCredentialsProvider(
+    credentialsProvider: ICredentialsProvider
+  ): void {
+    this.credentialsProvider = this.credentialsProvider || credentialsProvider;
   }
 
   private reserveForActor(actor: UsesAbilities): void {
@@ -29,8 +44,16 @@ export class Authenticate implements Ability {
     );
   }
 
+  public static asPersonaCalled(
+    personaName: string,
+    personaManager: PersonaManager = context.personaManager
+  ): Authenticate {
+    return new Authenticate(personaName, personaManager);
+  }
+
   public static as(actor: UsesAbilities): Authenticate {
     const ability = actor.abilityTo(Authenticate);
+    ability.defaultCredentialsProvider(new LightCredentialsProvider());
     ability.reserveForActor(actor);
     return ability;
   }
--- test/screenplay/abilities/authenticate.test.ts
+++ test/screenplay/abilities/authenticate.test.ts
@@ -1,6 +1,12 @@
 import { chai } from "@/../test/chai-extra";
 import { context } from "@/context";
 import { Persona, PersonaManager } from "@/persona";
+import {
+  BasicCredentials,
+  Credentials,
+  ICredentialsProvider,
+  LightCredentialsProvider,
+} from "@/persona/auth";
 import { Authenticate } from "@/screenplay/abilities/authenticate";
 import { ConfigurationError, serenity } from "@serenity-js/core";
 import { Actor, actorCalled } from "@serenity-js/core";
@@ -15,6 +21,7 @@ describe("AuthenticateWithCredentials ability", () => {
   const sandbox = createSandbox();
   let personaManager: PersonaManager;
   let actor;
+  let credentialProvider: ICredentialsProvider;
 
   beforeEach(() => {
     personaManager = {
@@ -23,8 +30,14 @@ describe("AuthenticateWithCredentials ability", () => {
       addPersona: sandbox.stub(),
       reservePersonaFor: sandbox.stub(),
     };
+    credentialProvider = {
+      getCredentialsFor: sandbox.stub(),
+    };
     actor = actorCalled("Actor").whoCan(
-      Authenticate.asPersonaCalled("Sales", personaManager)
+      Authenticate.asPersonaCalled(
+        "Sales",
+        personaManager
+      ).withCredentialsProvider(credentialProvider)
     );
   });
 
@@ -60,5 +73,36 @@ describe("AuthenticateWithCredentials ability", () => {
 
     const ability = Authenticate.as(actor);
     ability.persona().should.be.equal(expectedPersona);
+  });
+
+  it("resolves credntials", () => {
+    const actor = actorCalled("testActor").whoCan(
+      Authenticate.asPersonaCalled(
+        "Sales",
+        personaManager
+      ).withCredentialsProvider(credentialProvider)
+    );
+    const expectedCredentials: Credentials = new BasicCredentials(
+      "username",
+      "passwd"
+    );
+    (credentialProvider.getCredentialsFor as SinonStub).returns(
+      expectedCredentials
+    );
+
+    const ability = Authenticate.as(actor);
+
+    ability.credentials().should.be.deep.equal(expectedCredentials);
+  });
+
+  it("checks credential provider existence when .as(actor) is called", () => {
+    const actor = actorCalled(
+      "TestActor With Ability Without CredentialsProvider"
+    ).whoCan(Authenticate.asPersonaCalled("Sales", personaManager));
+
+    const ability = Authenticate.as(actor);
+
+    ((ability as any).credentialsProvider instanceof LightCredentialsProvider)
+      .should.be.true;
   });
 });
